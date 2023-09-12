e0f95c34a88e5e2cee14adf952aef14003a57470
Matias Kudaka <111797730+maticba@users.noreply.github.com>
2023-09-12 17:21:44 -0300

    Seguimos trabajando con Python
    Profundizando con Sistemas Numéricos y String

---
+++ Programación IV/Clase01/tipo_bool.py
@@ -0,0 +1,11 @@
+#Bool contiene los valores true y false
+# Los tipos numericos, es false para 0 (cero), true para los demas valores
+
+
+valor = 0.0
+resultado = bool (valor)
+print(f'Valor: {valor}, Resultado: {resultado}')
+
+valor = 0.1
+resultado = bool (valor)
+print(f'Valor: {valor}, Resultado: {resultado}')
