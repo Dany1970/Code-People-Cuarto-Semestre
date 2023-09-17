c5126a69bcb242c51f5243acf3df7f856e05b9e0
Martin Torres <103221160+MartinT2222@users.noreply.github.com>
2023-09-16 21:29:39 -0300

    poniendo en función la primera plantilla
    Co-Authored-By: Marianela <73837184+Marianela00@users.noreply.github.com>

--- PPS/consultas_on_demand/practica_profecional/practica_profecional/settings.py
+++ PPS/consultas_on_demand/practica_profecional/practica_profecional/settings.py
@@ -124,8 +124,8 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
-    '/var/www/static'
+    BASE_DIR / 'static',
+    '/var/www/static',
 ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
