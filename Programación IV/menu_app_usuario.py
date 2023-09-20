4a4717c9ed7deaf7fb7809e7ed9f1d4ced3b2db3
Beatriz <111789906+CreatemyGithub19@users.noreply.github.com>
2023-09-20 15:25:17 -0300

    commitear lo que falta
    añadir al repo

--- Programación IV/menu_app_usuario.py
+++
@@ -1,9 +0,0 @@
-opcion = None
-while opcion != 5:
-    print("Opciones":)
-    print("1.Agregar Usuario")
-    print("2.Modificar Usuario")
-    print("4.Eliminar Usuario")
-    print("5.Salir")
-    
-opcion = int(input("Digite la opcion (1-5): "))
