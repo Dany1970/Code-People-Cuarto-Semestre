765eaecb3e3a1bef9e192a55a4db4bf55991bdb5
Beatriz <111789906+CreatemyGithub19@users.noreply.github.com>
2023-09-20 15:35:12 -0300

    e-commerce2022 añado artículos
    añadir al repositorio de github

---
+++ e-commerce2022/client/js/products.js
@@ -0,0 +1,80 @@
+const productos = [
+    {
+        id: 1,
+        productName: "aromatizante de ambiente",
+        price: 350,
+        quanty: 1,
+        img: "/client/media/aromatizante de ambiente.png",
+
+    },
+    {
+        id: 2,
+        productName: "Cera autobrillante",
+        price: 6000,
+        quanty: 1,
+        img: "/client/media/Cera-fuller-autobrillante-de-2-litros.jpg",
+
+    },
+    {
+        id: 3,
+        productName: "desodorante de ambiente",
+        price: 800,
+        quanty: 1,
+        img: "/client/media/desodorantedeambiente.jpg",
+
+    },
+    {
+        id: 4,
+        productName: "difusor aromatizante",
+        price: 600,
+        quanty: 1,
+        img: "/client/media/difusoraromatizante.jpg",
+    
+    },
+    {
+        id: 5,
+        productName: "escobillon",
+        price: 600,
+        quanty: 1,
+        img: "/client/media/escobillon.jpg",
+
+    },
+    {
+        id: 6,
+        productName: "guante de limpieza",
+        price: 200,
+        quanty: 1,
+        img: "/client/media/guante de limpieza.png",
+
+    },
+    {
+        id: 7,
+        productName: "lavandina",
+        price: 500,
+        quanty: 1,
+        img: "/client/media/lavandina.jpg",
+    },
+    {
+        id: 8,
+        productName: "papel higiénico",
+        price: 350,
+        quanty: 1,
+        img: "/client/media/papelhigienico.jpg",
+    },
+    {
+        id: 9,
+        productName: "perfume para piso",
+        price: 500,
+        quanty: 1,
+        img: "/client/media/perfumeparapiso",
+    },
+    {
+        id: 10,
+        productName: "shampoo para autos";
+        price: 2000,
+        quanty: 1,
+        img: "/client/media/shapoo para autos.png",
+    },
+    {
+
+        
