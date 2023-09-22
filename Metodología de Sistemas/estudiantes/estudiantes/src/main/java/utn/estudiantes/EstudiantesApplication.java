6019cd218978aaa16c3c569dfea58748e694f672
Daniela Armijo <111472176+Dany1970@users.noreply.github.com>
2023-09-21 22:16:05 -0300

    Correcciones y agregados
    Se realizan correciones en el código de estudiante, debido a un error de incompatibilidad con la DB y se agrega la opción 1 del menú

--- Metodología de Sistemas/estudiantes/estudiantes/src/main/java/utn/estudiantes/EstudiantesApplication.java
+++ Metodología de Sistemas/estudiantes/estudiantes/src/main/java/utn/estudiantes/EstudiantesApplication.java
@@ -1,13 +1,66 @@
 package utn.estudiantes;
+import java.util.Scanner;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import utn.estudiantes.servicio.EstudianteServicio;
+import utn.estudiantes.modelo.Estudiantes2022;
 
 @SpringBootApplication
-public class EstudiantesApplication {
+public class EstudiantesApplication implements  CommandLineRunner{
+    @Autowired
+    private EstudianteServicio estudianteServicio;
+    private static final Logger logger =
+            LoggerFactory.getLogger(EstudiantesApplication.class);
 
-	public static void main(String[] args) {
-		SpringApplication.run(EstudiantesApplication.class, args);
-	}
+    String nl = System.lineSeparator();
+    public static void main(String[]args) {
+        logger.info("Iniciando la aplicación...");
+        //Levantar la fábrica de Spring
+        SpringApplication.run(EstudiantesApplication.class, args);
+        logger.info("Aplicación finalizada!");
+    }
 
+
+
+    @Override
+    public void run(String... args) throws Exception{
+        logger.info("Ejecutando el método run de Spring..." + nl);
+        var salir = false;
+        var consola = new Scanner(System.in);
+        while (!salir) {
+            mostrarMenu();
+            salir = ejecutarOpciones(consola);
+            logger.info(nl);
+        }//Fin ciclo while
+    }
+
+    private void mostrarMenu() {
+        logger.info(nl);
+        logger.info("""
+				******* Sistema de Estudiantes *******
+				1. Listar Estudiantes
+				2. Buscar Estudiante
+				3. Agregar Estudiante
+				4. Modificar Estudiante
+				5. Eliminar Estudiante
+				6. Salir
+				Elija una opción:""");
+    }
+    private boolean ejecutarOpciones(Scanner consola) {
+        var opcion = Integer.parseInt(consola.nextLine());
+        var salir = false;
+        switch (opcion) {
+            case 1 -> {//Listar Estudiantes
+                logger.info(nl + "Listado de estudiantes: " + nl);
+                List<Estudiantes2022> estudiantes = estudianteServicio.listarEstudiantes();
+                estudiantes.forEach((estudiante -> logger.info(estudiante.toString() + nl)));
+            }
+        }//Fin switch
+        return salir;
+    }
 }
--- Metodología de Sistemas/estudiantes/estudiantes/src/main/java/utn/estudiantes/servicio/IEstudianteServicio.java
+++ Metodología de Sistemas/estudiantes/estudiantes/src/main/java/utn/estudiantes/servicio/IEstudianteServicio.java
@@ -1,10 +1,12 @@
 package utn.estudiantes.servicio;
 
 import utn.estudiantes.modelo.Estudiante;
+import utn.estudiantes.modelo.Estudiantes2022;
 
 public interface IEstudianteServicio {
-    public List<Estudiante> listarEstudiantes();
-    public Estudiante buscarEstudiantePorId(Integer idEstudiante);
-    public void elimnarEstudiante(Estudiante estudiante);
+    public List<Estudiantes2022>listarEstudiantes();
+    public Estudiantes2022 buscarEstudiantePorId(Integer idEstudiantes2022);
+    public void elimnarEstudiante(Estudiantes2022 estudiantes2022);
+    public void guardarEstudiante(Estudiantes2022 estudiante);
 
 }
