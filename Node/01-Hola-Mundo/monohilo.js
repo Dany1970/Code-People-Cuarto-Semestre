5518ed5a87617d5f37ab63b52043da886fbb2478
Matu8990 <102619951+Matu8990@users.noreply.github.com>
2023-10-28 14:05:35 -0300

    Node
    Clase 07 Herramientas de desarrollo y producción
    
    Hoy vamos a avanzar en lo que son las herramientas que podemos usar en el desarrollo y en producción
    
    7.1 Nodemon -> herramienta para el desarrollo
    
    Instalación
    
    Teclear aquí para VER el video 1
    
    Documentación
    
    7.2 Problemas con Window -> ejecución de nodemon
    
    Enlace
    
    Teclear aquí para VER el video 2
    
    7.3 PM2 -> herramienta para producción
    
    Instalación
    
    Teclear aquí para VER el video 3
    
    7.4 Ejecución de PM2
    
    Parte -> 1
    
    Teclear aquí para VER el video 4
    
    Parte -> 2
    
    Teclear aquí para VER el video 5

---
+++ Node/01-Hola-Mundo/monohilo.js
@@ -0,0 +1,12 @@
+console.log("Hola a todos!")
+
+var i = 0;
+setInterval(function(){
+    console.log(i);
+    i++;
+    //if(i === 5){
+        //console.log('Forzamos el error')
+        //var a = 3 + z;
+    //}
+},1000);
+console.log("Segunda instrucción")
